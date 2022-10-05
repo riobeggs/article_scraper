6bd939d1288af0d59cd764d8e02ca18565274e23
Rio <riobeggs1@gmail.com>
2022-10-06 02:48:58 +1300

    called createtxtfiel method

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from assets.article_scraper import Article
 def main():
     ar = Article()
     ar.scrape_text()
-    print(ar.article_text)
-
+    ar.scrape_title()
+    ar.create_txt_file()
 
 if __name__ == "__main__":
     main()
