f6d7e4cb062750ef00f5a8179a2ac6ef68041e32
Rio <riobeggs1@gmail.com>
2022-10-05 15:02:44 +1300

    added decoding statement

--- assets/webdriver.py
+++ assets/webdriver.py
@@ -5,6 +5,7 @@ INVALID_URL = True
 def run_web_driver():
     while INVALID_URL:
         url = input("Enter URL: ")
+        print("\nDecoding...\n")
         try:
             DRIVER.get(url)
             return DRIVER
