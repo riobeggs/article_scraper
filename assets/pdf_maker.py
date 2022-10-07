f4c33ff6e61f9e7212f7204ddc884371d4ecc853
Rio <riobeggs1@gmail.com>
2022-10-07 23:53:03 +1300

    changed font to unicode font that supports utf8 encoding. regex replace anything in title that isnt in alphabet

--- assets/pdf_maker.py
+++ assets/pdf_maker.py
@@ -1,3 +1,4 @@
+import re
 from fpdf import FPDF
 
 
@@ -8,26 +9,25 @@ def make_pdf(article_title: str, article_text: str):
     Returns the file name as a string
     """
 
-    article_text = article_text.encode("latin-1", "replace").decode("latin-1")
-
     pdf = FPDF()
-    pdf.set_top_margin(30)
+    pdf.add_font("TNR", "", r"assets/fonts/times new roman.ttf")
+    pdf.set_margins(30,30,30)
     pdf.add_page()
 
-    pdf.set_left_margin(30)
-
     # create title
-    pdf.set_font("Times", size=26)
+    pdf.set_font("TNR", size=26)
     pdf.multi_cell(150, 10, txt=article_title, align="L")
 
     # insert line break
     pdf.cell(150, 5, ln=2)
 
     # create text
-    pdf.set_font("Times", size=12)
+    pdf.set_font("TNR", size=12)
+    pdf.set_y(pdf.get_y())
     pdf.multi_cell(150, 10, txt=article_text, align="L")
 
-    article_title = article_title.replace(" ", "").replace("-", "")
+    article_title = re.sub(r"[^a-zA-Z0-9]", "|", article_title)
+    article_title = article_title.replace(" ", "")
 
     # save the pdf as article_title.pdf
     file = f"{article_title}.pdf"
