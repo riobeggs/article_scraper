70abf9dd0a0fb22abc71dbfab5e14312af375ae1
Rio <riobeggs1@gmail.com>
2022-10-07 22:34:25 +1300

    removed txt file creator and added doc strings for methods

--- assets/article_scraper.py
+++ assets/article_scraper.py
@@ -4,27 +4,39 @@ import re
 
 
 class Article:
+    _driver = None
     _html = None
     _article_text = None
+    _article_name = None
     _text_list = None
 
     def __init__(self, driver=Driver.run_web_driver()):
         self._driver = driver
         self._html = BeautifulSoup(self._driver.page_source, "html.parser")
         self._article_text = None
-        self._text_list = []
         self._article_name = None
+        self._text_list = []
 
     def scrape_title(self) -> str:
+        """
+        Scrapes given url for article title.
+
+        Returns article title as a string.
+        """
         find_heading = self._html.find(class_="article__heading")
         for heading in find_heading:
             heading = str(heading)
             self._article_name = re.sub(re.compile("<.*?>"), "", heading)
+            self._article_name = self._article_name.capitalize()
         return self._article_name
 
     def scrape_text(self) -> str:
+        """
+        Scrapes given url for article title.
+
+        Returns article title as a string.
+        """
         subscription_text = "$1.99per week Share this article Reminder, this is a Premium article and requires a subscription to read."
-        # Scrape article text
         find_article = self._html.find_all(class_="article__body")
         content = find_article[0].find_all("p")
         for line in content:
@@ -35,11 +47,6 @@ class Article:
         self._article_text = " ".join(self._text_list)
         self._article_text = self._article_text.replace(subscription_text, "").strip()
         return self._article_text
-
-    def create_txt_file(self):
-        path = (f"/Users/riobeggs/Downloads/{self._article_name}.txt")
-        article_text_file = open(path, 'w')
-        article_text_file.write(self._article_text)
 
     @property
     def article_text(self):
