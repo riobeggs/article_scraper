872000bb428ab25fd7e766cf3704abdcb97d3803
Rio <riobeggs1@gmail.com>
2022-10-07 22:27:11 +1300

    added pdf maker and pdf opener to app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from assets.article_scraper import Article
+from assets.pdf_maker import make_pdf
+from assets.open_article import read_article
 
 
 def main():
     ar = Article()
     ar.scrape_text()
     ar.scrape_title()
-    ar.create_txt_file()
+    article_pdf = make_pdf(ar.article_name, ar.article_text)
+    read_article(article_pdf)
 
 if __name__ == "__main__":
     main()
