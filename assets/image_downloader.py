5a5938d83ca8c12d3a4b99212d344230135caee0
Rio <riobeggs1@gmail.com>
2022-10-12 01:05:15 +1300

    replaced path with relative path

--- assets/image_downloader.py
+++ assets/image_downloader.py
@@ -5,7 +5,7 @@ import re
 def download_image(title, url) -> str:
     if isinstance(url, str):
         title = re.sub(r"[^a-zA-Z0-9]", "", title)
-        image_file_path = f"/Users/riobeggs/Documents/code/nzherald_webscraper/assets/images/{title}.jpg"
+        image_file_path = f"./assets/images/{title}.jpg"
         urllib.request.urlretrieve(url, image_file_path)
 
         return image_file_path
