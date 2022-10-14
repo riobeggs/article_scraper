d6570bd18946f25f3b804779f1ff484018b07f97
Rio <riobeggs1@gmail.com>
2022-10-14 23:26:30 +1300

    now runs webserver aswell as previous functions

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from assets.article_scraper import Article
 from assets.pdf_maker import make_pdf
-from assets.open_article import read_article
+import os
+from flask import Flask, render_template, request, send_from_directory
 
 
-def main():
-    ar = Article()
-    article_pdf = make_pdf(ar.article_title, ar.article_text, ar.article_image)
-    read_article(article_pdf)
+app = Flask(__name__)
+
+
+@app.route("/", methods=("GET", "POST"))
+def home():
+    if request.method == "GET":
+        return render_template("home.html")
+
+    if request.method == "POST":
+        url = request.form["url"]
+
+        ar = Article(url)
+        article_name = make_pdf(ar.article_title, ar.article_text, ar.article_image)
+
+        workingdir = os.path.abspath(os.getcwd())
+        filepath = workingdir + '/assets/articles'
+        return send_from_directory(filepath, article_name)
+
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
