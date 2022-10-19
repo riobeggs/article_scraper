886cbf44decb9106f1eef4e1f9672daceca97a73
Rio <riobeggs1@gmail.com>
2022-10-19 16:19:24 +1300

    added doc strings and added pdf maker method to article class

--- assets/article_scraper.py
+++ assets/article_scraper.py
@@ -4,17 +4,11 @@ import tempfile
 import urllib.request
 
 from bs4 import BeautifulSoup
+from fpdf import FPDF
 from selenium import webdriver
 
 
 class Article:
-    _html = None
-    _article_text = None
-    _article_title = None
-    _text_list = None
-    _article_image = None
-    _tmpdir = None
-
     def __init__(self, url: str):
 
         driver = webdriver.Safari()
@@ -25,15 +19,19 @@ class Article:
         self._article_title = None
         self._text_list = []
         self._article_image = None
+        self._cwd = None
         self._tmpdir = None
+        self._pdf_name = None
 
-    def make_tmpdir(self):
-        cwd = os.path.abspath(os.getcwd())
-        tmpdirpath = cwd + "/assets"
+    def _make_tmpdir(self):
+        """
+        Sets up temporary directory
+        """
+        self._cwd = os.path.dirname(__file__)
 
-        self._tmpdir = tempfile.TemporaryDirectory(dir=tmpdirpath)
+        self._tmpdir = tempfile.TemporaryDirectory(dir=self._cwd)
 
-    def scrape_title(self):
+    def _scrape_title(self):
         """
         Scrapes given url for article title.
         """
@@ -41,12 +39,12 @@ class Article:
             find_heading = self._html.find(class_="article__heading")
             for heading in find_heading:
                 heading = str(heading)
-                self._article_title = re.sub(re.compile("<.*?>"), "", heading)
+                self._article_title = re.sub(re.compile(r"<.*?>"), "", heading)
                 self._article_title = self._article_title.capitalize()
         except:
             pass
 
-    def scrape_text(self):
+    def _scrape_text(self):
         """
         Scrapes given url for article title.
         """
@@ -60,11 +58,13 @@ class Article:
                 text = text.replace("\n", " ")
                 self._text_list.append(text)
             self._article_text = " ".join(self._text_list)
-            self._article_text = self._article_text.replace(subscription_text, "").strip()
-        except: 
+            self._article_text = self._article_text.replace(
+                subscription_text, ""
+            ).strip()
+        except:
             pass
 
-    def scrape_image(self):
+    def _scrape_image(self):
         """
         Scrapes given url for article header image.
         """
@@ -79,45 +79,92 @@ class Article:
                 pattern = re.compile(r"^.*?\.jpg")
                 image_url = pattern.findall(image)
 
-                self._article_image = self.download_image(image_url[0])
+                self._article_image = self._download_image(image_url[0])
 
                 break
         except:
             pass
 
-    def download_image(self, url) -> str:
-        if isinstance(url, str):
-            self._article_title = re.sub(r"[^a-zA-Z0-9]", "", self._article_title)
+    def _download_image(self, url) -> str:
+        """
+        Downloads article header image to tmpdir.
+        """
+        assert isinstance(
+            url, str
+        ), f"url is the wrong type, expected string but got {type(url)}"
+        self._article_title = re.sub(r"[^a-zA-Z0-9]", "", self._article_title)
 
-            image_file_path = f"{self._tmpdir.name}/{self._article_title}.jpg"
-            urllib.request.urlretrieve(url, image_file_path)
+        image_file_path = f"{self._tmpdir.name}/{self._article_title}.jpg"
+        urllib.request.urlretrieve(url, image_file_path)
 
-            return image_file_path
+        return image_file_path
+
+    def _make_pdf(self) -> str:
+        """
+        Creates a pdf file containing the chosen article.
+
+        Returns the file name as a string
+        """
+
+        pdf = FPDF()
+
+        tnr = "Times New Roman"
+        tnrb = "Times New Roman Bold"
+
+        pdf.add_font(tnr, "", f"{self._cwd}/fonts/times new roman.ttf", uni=True)
+        pdf.add_font(tnrb, "", f"{self._cwd}/fonts/times new roman bold.ttf", uni=True)
+        pdf.set_margins(30, 23, 30)
+        pdf.add_page()
+
+        # create title
+        if isinstance(self._article_title, str):
+            pdf.set_font(tnrb, size=28)
+            pdf.multi_cell(150, 12, txt=self._article_title, align="L")
+
+        if isinstance(self._article_image, str):
+            # insert image
+            pdf.cell(150, 5, ln=2)
+            pdf.set_y(pdf.get_y())
+            pdf.image(self._article_image, w=(pdf.w - 60))
+            pdf.cell(150, 5, ln=2)
+        else:
+            # insert line break
+            pdf.set_font(tnrb, size=28)
+            pdf.cell(150, 7, ln=2)
+
+        # create text
+        # TODO change to aa font that supports macrons
+        if isinstance(self._article_text, str):
+            pdf.set_font(tnr, size=12)
+            pdf.set_y(pdf.get_y())
+            pdf.multi_cell(150, 8, txt=self._article_text, align="L")
+
+        if self._article_title == None:
+            self._article_title = "article"
+
+        self._article_title = re.sub(r"[^a-zA-Z0-9]", "", self._article_title)
+        self._article_title = self._article_title.replace(" ", "")
+
+        path = f"{self._tmpdir.name}/{self._article_title}.pdf"
+        pdf.output(path)
+
+        # returns the file name as a string
+        self._pdf_name = f"{self._article_title}.pdf"
+
+    def run(self):
+        """
+        Runs methods required for gathering article content.
+        """
+        self._make_tmpdir()
+        self._scrape_title()
+        self._scrape_text()
+        self._scrape_image()
+        self._make_pdf()
 
     @property
     def tmpdir(self):
-        if not self._tmpdir:
-            self.make_tmpdir()
-
         return self._tmpdir
 
     @property
-    def article_title(self):
-        if not self._article_title:
-            self.scrape_title()
-
-        return self._article_title
-
-    @property
-    def article_text(self):
-        if not self._article_text:
-            self.scrape_text()
-
-        return self._article_text
-
-    @property
-    def article_image(self):
-        if not self._article_image:
-            self.scrape_image()
-
-        return self._article_image
+    def pdf_name(self):
+        return self._pdf_name
