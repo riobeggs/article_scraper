7c917460934d10573e649bd656494360e7f50da0
Rio <riobeggs1@gmail.com>
2022-10-30 00:16:44 +1300

    renamed. Have issue with relative importing

--- tests/article_scraper.py
+++
@@ -1,9 +0,0 @@
-import logging
-import os
-import unittest
-from unittest.mock import MagicMock, Mock
-
-from assets.article_scraper import Article
-from bs4 import BeautifulSoup
-
-logging.basicConfig(level=logging.DEBUG)
---
+++ tests/article_scraper_unittest.py
@@ -0,0 +1,37 @@
+import logging
+import os
+import unittest
+from unittest.mock import MagicMock, mock_open
+
+from assets.article_scraper import Article
+from bs4 import BeautifulSoup
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class ArticleScraper(unittest.TestCase):
+    def setUp(self) -> None:
+        logging.debug(f"Setting Up - {self._testMethodName}")
+        self.scraper = Article()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        logging.debug("Tearing Down")
+        return super().tearDown()
+
+    def test_init(self):
+        """
+        Pass in a file path and check that it loads files.
+        """
+        self.assertIsInstance(self.scraper._html, BeautifulSoup)
+        self.assertIsNone(self.scraper._article_text)
+        self.assertIsNone(self.scraper._article_title)
+        self.assertIsInstance(self.scraper._text_list, list)
+        self.assertIsNone(self.scraper._article_image)
+        self.assertIsNone(self.scraper._cwd)
+        self.assertIsNone(self.scraper._tmpdir)
+        self.assertIsNone(self.scraper._pdf_name)
+
+
+if __name__ == "__main__":
+    unittest.main()
