faa207d4093727d76d88f82f52cd1f8a41e4aa3f
Rio <riobeggs1@gmail.com>
2022-11-01 00:06:51 +1300

    started working on invalid url tester

--- tests/article_scraper_unittest.py
+++ tests/article_scraper_unittest.py
@@ -2,9 +2,9 @@ import logging
 import os
 import sys
 import unittest
-from unittest.mock import MagicMock, mock_open
 
 from bs4 import BeautifulSoup
+from selenium.common.exceptions import InvalidArgumentException
 
 sys.path.append(os.path.abspath("."))
 from assets.article_scraper import Article
@@ -15,16 +15,17 @@ logging.basicConfig(level=logging.DEBUG)
 class ArticleScraper(unittest.TestCase):
     def setUp(self) -> None:
         logging.debug(f"Setting Up - {self._testMethodName}")
-        self.scraper = Article()
+        self._exampleURL = "https://tinyurl.com/mms6y95d"
+        self.scraper = Article(url=self._exampleURL)
         return super().setUp()
 
     def tearDown(self) -> None:
         logging.debug("Tearing Down")
         return super().tearDown()
 
-    def test_init(self):
+    def test_init_loads_url(self):
         """
-        Pass in a file path and check that it loads files.
+        Pass in a url and check that it loads.
         """
         self.assertIsInstance(self.scraper._html, BeautifulSoup)
         self.assertIsNone(self.scraper._article_text)
@@ -34,6 +35,19 @@ class ArticleScraper(unittest.TestCase):
         self.assertIsNone(self.scraper._cwd)
         self.assertIsNone(self.scraper._tmpdir)
         self.assertIsNone(self.scraper._pdf_name)
+
+    def test_init_loads_invalid_url(self):
+        """
+        Tests that an invalid url passed in throws an error.
+        """
+        # TODO: Does not work for some reason
+        with self.assertRaises(InvalidArgumentException) as error_context:
+            self.scraper = Article("invalid_url")
+
+            actual_error_msg = str(error_context.exception)
+            expected_error_msg = "Could not parse requested URL 'invalid_url'"
+
+            self.assertEqual(actual_error_msg, expected_error_msg)
 
 
 if __name__ == "__main__":
