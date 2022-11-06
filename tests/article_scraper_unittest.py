c6a4a2daa3a3c826a11dc8eee4cf41b1a9c5a2c7
Rio <riobeggs1@gmail.com>
2022-11-07 12:02:25 +1300

    Created new tests *Removed textlist from init checking *Added testing for creation of tmpdir *Added testing for title scraping *Added tesing for text scraping *Added testing for image scraping

--- tests/article_scraper_unittest.py
+++ tests/article_scraper_unittest.py
@@ -18,8 +18,8 @@ class ArticleScraper(unittest.TestCase):
         self.logger.debug(f"Setting Up - {self._testMethodName}")
 
         self._exampleURL = "https://tinyurl.com/mms6y95d"
-        self.scraper = Article(url=self._exampleURL)
-        
+        self.scraper = Article(self._exampleURL)
+
         return super().setUp()
 
     def tearDown(self) -> None:
@@ -31,11 +31,11 @@ class ArticleScraper(unittest.TestCase):
         Pass in a url and check that it loads.
         """
         self.assertIsInstance(self.scraper._html, BeautifulSoup)
-        self.assertIsNone(self.scraper._article_text)
         self.assertIsNone(self.scraper._article_title)
-        self.assertIsInstance(self.scraper._text_list, list)
+        self.assertIsNone(self.scraper._article_text)
         self.assertIsNone(self.scraper._article_image)
         self.assertIsNone(self.scraper._cwd)
+        self.assertIsNone(self.scraper._image_url)
         self.assertIsNone(self.scraper._tmpdir)
         self.assertIsNone(self.scraper._pdf_name)
 
@@ -50,6 +50,59 @@ class ArticleScraper(unittest.TestCase):
         expected_error_msg = "Message: Could not parse requested URL 'invalid_url'\n"
 
         self.assertEqual(actual_error_msg, expected_error_msg)
+
+    def test_tmpdir(self):
+        """
+        Test that a temporary directory has been created.
+        """
+        # Act
+        self.scraper._make_tmpdir()
+
+        # Assert
+        self.assertIsInstance(self.scraper.tmpdir.name, str)
+        self.scraper.tmpdir.cleanup()
+
+    def test_scrape_title(self):
+        """
+        Scrape the title from our test asset and assert that its working as intended
+        """
+        # Arrange
+        expected_title = (
+            "Fulton Hogan staff take up shares as dividend jumps 46 per cent"
+        )
+
+        # Act
+        self.scraper._scrape_title()
+        actual_title = self.scraper.title
+
+        # Assert
+        self.assertEqual(actual_title, expected_title)
+
+    def test_scrape_text(self):
+        """
+        Scrape the text from our test asset and assert that its working as intended
+        """
+        # Act
+        self.scraper._scrape_text()
+
+        # Assert
+        self.assertIsInstance(self.scraper.text, str)
+
+    def test_scrape_image(self):
+        """
+        Scrape the image from our test asset and assert that its working as intended        
+        """
+        # Arrange
+        self.scraper._make_tmpdir()
+        self.scraper._scrape_title()
+        self.scraper._scrape_text()
+
+        # Act
+        self.scraper._scrape_image_url()
+
+        # Assert
+        self.assertIsInstance(self.scraper.image, str)
+        self.scraper.tmpdir.cleanup()
 
 
 if __name__ == "__main__":
