0bde4ead397d1c9a42038007b7e45c99c97e80bf
Rio <riobeggs1@gmail.com>
2022-11-08 19:32:49 +1300

    closing tmpdir after termination

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+import tempfile
+
 from flask import Flask, render_template, request, send_from_directory
 
 from assets.article_scraper import Article
@@ -14,8 +17,14 @@ def home():
         url = request.form["url"]
 
         ar = Article(url)
-        ar.run()
-        return send_from_directory(ar.tmpdir.name, ar.pdf_name)
+
+        # TODO: closing tmpdir after termination
+        cwd = os.path.dirname(__file__)
+        with tempfile.TemporaryDirectory(dir=cwd) as tmpdir:
+            ar._tmpdir = tmpdir
+            ar._cwd = cwd
+            ar.run()
+            return send_from_directory(ar.tmpdir.name, ar.pdf_name)
 
 
 if __name__ == "__main__":
