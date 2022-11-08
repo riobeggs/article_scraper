4a8afb5999befc1ae92fb464d54c2d04dbd5488e
Rio <riobeggs1@gmail.com>
2022-11-09 01:21:51 +1300

    removed unused imports

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import os
-import tempfile
-
 from flask import Flask, render_template, request, send_from_directory
 
 from assets.article_scraper import Article
@@ -11,20 +8,17 @@ app = Flask(__name__)
 @app.route("/", methods=("GET", "POST"))
 def home():
     if request.method == "GET":
+
         return render_template("home.html")
 
     if request.method == "POST":
+
         url = request.form["url"]
 
         ar = Article(url)
+        ar.run()
 
-        # TODO: closing tmpdir after termination
-        cwd = os.path.dirname(__file__)
-        with tempfile.TemporaryDirectory(dir=cwd) as tmpdir:
-            ar._tmpdir = tmpdir
-            ar._cwd = cwd
-            ar.run()
-            return send_from_directory(ar.tmpdir.name, ar.pdf_name)
+        return send_from_directory(ar.tmpdir.name, ar.pdf_name)
 
 
 if __name__ == "__main__":
